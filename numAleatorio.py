85c7ad8e6190413c9b5c189b6bb0afc53dabaa8f
minoreh <minoreherrera@gmail.com>
2016-07-31 15:59:22 -0600

    contador de intentos del usuario
    De continuar una segunda vez el juego, se debe desplegar cuántos juegos
    ha jugado el usuario.

--- numAleatorio.py
+++ numAleatorio.py
@@ -1,7 +1,10 @@
-#encoding: utf-8
+# -*- coding: utf-8 -*-
 import random
 
-while salir < 2:
+juego=0
+validacion = 0
+salir=1
+while salir <2:
     juego = int( juego ) + 1
     print( "juego No:" + str( juego ) )
     conta = 0
@@ -9,10 +12,10 @@ while salir < 2:
     azar = random.randint( 10, 101 )
     print( azar )
     print( "Ingresa un numero entero entre (10-101):" )
-    while int( conta ) < 5:
-        conta = int( conta ) + 1
+    while int(conta) < 5:
+        conta = int(conta) + 1
         print( "Intento No:" + str( conta ) )
-        num1 = input( )
+        num1 = input()
         num1 = int( num1 )
         if num1 >= 10 and num1 <= 101:
             print( "Digita otro numero:" )
@@ -21,6 +24,7 @@ while salir < 2:
             if num1 > azar:
                 print( ">El numero debe ser menor..." )
             if num1 == azar and conta == 1:
+
                 print( "FELICIDADES, HAS LOGRADO ADIVINAR A LA PRIMERA " )
                 break
             if num1 == azar:
@@ -32,6 +36,7 @@ while salir < 2:
         else:
             print( "ERROR:Debe digitar un numero en el rango(10-101)" )
 
+
     print( "DESEA JUGAR OTRA VEZ? DIGITE UN: 1:(SI) 2:(N0)" )
     deci = input( )
     deci = int( deci )
@@ -40,4 +45,7 @@ while salir < 2:
         salir = 3
     elif deci == 1:
         salir = 1
-    
+    else:
+        print( "ERROR:DIGITE UN NUMERO VALIDO" )
+        salir = 3
+
