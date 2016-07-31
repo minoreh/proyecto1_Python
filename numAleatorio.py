81e8a75d4115aaf80784934fc94f11bf53650b32
minoreh <minoreherrera@gmail.com>
2016-07-31 15:50:12 -0600

    arreglo contador de intentos, num aleatorio,
    el programa muestra un mensaje indicando que debe escribir un número
    menor o si es lo contrario el programa debe indicarle al usuario que
    escriba un número mayor. El usuario tiene la posibilidad de realizar 5
    intentos, y debe poder visualizar, cuántos intentos le quedan
    disponibles; cuando los intentos se agotan debe mostrar un mensaje que
    diga: "PERDISTE SE ACABARON TUS 5 INTENTOS". Pero, si en uno de los 5
    intentos el usuario averigua el número, el programa muestra el mensaje
    "GANASTE ADIVINASTE EL NUMERO ALEATORIO, EN EL INTENTO NÚMERO #", de
    adivinarlo a la primera debe decir: “FELICIDADES, HAS LOGRADO ADIVINAR A
    LA PRIMERA” y el juego finaliza. Al finalizar el juego, el usuario debe
    escoger si desea jugar otra vez o si desea salir del juego. De querer
    jugar de nuevo, el juego comienza desde cero. De continuar una segunda
    vez, se debe desplegar cuántos juegos ha jugado el usuario.

--- numAleatorio.py
+++ numAleatorio.py
@@ -1,13 +1,18 @@
 #encoding: utf-8
 import random
 
+while salir < 2:
+    juego = int( juego ) + 1
+    print( "juego No:" + str( juego ) )
+    conta = 0
+    deci = 0
     azar = random.randint( 10, 101 )
     print( azar )
     print( "Ingresa un numero entero entre (10-101):" )
-    while int(conta) < 5:
-        conta = int(conta) + 1
+    while int( conta ) < 5:
+        conta = int( conta ) + 1
         print( "Intento No:" + str( conta ) )
-        num1 = input()
+        num1 = input( )
         num1 = int( num1 )
         if num1 >= 10 and num1 <= 101:
             print( "Digita otro numero:" )
@@ -16,7 +21,6 @@ import random
             if num1 > azar:
                 print( ">El numero debe ser menor..." )
             if num1 == azar and conta == 1:
-
                 print( "FELICIDADES, HAS LOGRADO ADIVINAR A LA PRIMERA " )
                 break
             if num1 == azar:
@@ -29,8 +33,11 @@ import random
             print( "ERROR:Debe digitar un numero en el rango(10-101)" )
 
     print( "DESEA JUGAR OTRA VEZ? DIGITE UN: 1:(SI) 2:(N0)" )
-    if deci == 1:
-    salir = 1
-    else:
-    print( "ERROR:DIGITE UN NUMERO VALIDO" )
-    salir = 3
+    deci = input( )
+    deci = int( deci )
+    if deci == 2:
+        print( "...JUEGO TERMINADO..." )
+        salir = 3
+    elif deci == 1:
+        salir = 1
+    
