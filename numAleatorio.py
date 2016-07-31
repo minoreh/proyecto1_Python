b841f7db910db07758ca46b261a34caf9f352e35
minoreh <minoreherrera@gmail.com>
2016-07-31 15:32:59 -0600

    Generar un valor aleatorio entre 10 y 101, guardarlo en una variable, luego pedirle al usuario que escriba un número y comparar el número ge
    Generar un valor aleatorio entre 10 y 101, guardarlo en una variable,
    luego pedirle al usuario que escriba un número y comparar el número
    generado aleatoriamente con el número que tecleó el usuario, si los
    valores son iguales el juego se acaba, y se despliega un mensaje
    felicitando la victoria;

--- numAleatorio.py
+++ numAleatorio.py
@@ -1,27 +1,29 @@
+#encoding: utf-8
 import random
 
-#var1 = int(raw_input("Ingresa un numero entero entre (10-101): "))
+    azar = random.randint( 10, 101 )
+    print( azar )
+    print( "Ingresa un numero entero entre (10-101):" )
+    while int(conta) < 5:
+        conta = int(conta) + 1
+        print( "Intento No:" + str( conta ) )
+        num1 = input()
+        num1 = int( num1 )
+        if num1 >= 10 and num1 <= 101:
+            print( "Digita otro numero:" )
+            if num1 < azar:
+                print( ">El numero debe ser mayor..." )
+            if num1 > azar:
+                print( ">El numero debe ser menor..." )
+            if num1 == azar and conta == 1:
 
-mi_lista = []
-
-for n in range(10, 101):
-    mi_lista.append(random.randint(10, 101))
-
-
-num_usuario = int(raw_input("Ingresa un numero entero entre (10-101): "))
-if (num_usuario >10 ) or (num_usuario <101):
-    print(" Debe digitar un numero en el rango(10-101)")
-
-else:
-    print("numero correcto")
-
-
-
-print "Lista:", mi_lista
-print "Maximo:", max(mi_lista)
-print "Minimo:", min(mi_lista)
-
-#imprimir un numero al azar
-azar = random.sample(mi_lista, 1)
-print (azar)
-
+                print( "FELICIDADES, HAS LOGRADO ADIVINAR A LA PRIMERA " )
+                break
+            if num1 == azar:
+                conta = str( conta )
+                print( "GANASTE ADIVINASTE EL NUMERO ALEATORIO , EN EL INTENTO NUMERO: " + conta )
+                break
+            if num1 != azar and conta == 5:
+                print( "PERDISTE SE ACABARON TUS 5 INTENTOS" )
+        else:
+            print( "ERROR:Debe digitar un numero en el rango(10-101)" )
